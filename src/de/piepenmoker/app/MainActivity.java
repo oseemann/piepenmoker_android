8923ec9395f079cc0148ebef4f7858e930dfa406
Oliver Seemann <oseemann@gmail.com>
2012-11-11 12:35:27 +0100

    Initial check-in of piepenmoker native android app
    The app is in simple "Hello, World!" state, showing two hard-coded pipes with
    image and name in a listview.

---
+++ src/de/piepenmoker/app/MainActivity.java
@@ -0,0 +1,55 @@
+package de.piepenmoker.app;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    public JSONObject[] getPipes() {
+        String json = "[{\"price\": 140, \"name\": \"PM-121005-b\", \"teaser\":\"http://www.piepenmoker.de/static/images/pipes/421/teaser/3322.jpg\"}, "
+        +"{\"price\": 120, \"name\":\"PM-120821-ax\", \"teaser\": \"http://www.piepenmoker.de/static/images/pipes/390/teaser/3071.jpg\"}]";
+
+        JSONObject[] pipes = null;
+        try {
+            JSONArray arr = (JSONArray) new JSONTokener(json).nextValue();
+            pipes = new JSONObject[arr.length()];
+            pipes[0] = (JSONObject)arr.get(0);
+            pipes[1] = (JSONObject)arr.get(1);
+        } catch (JSONException e) {
+
+        }
+        return pipes;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        JSONObject[] pipes = getPipes();
+
+        PipeArrayAdapter adapter = new PipeArrayAdapter(this,
+                android.R.layout.simple_list_item_1, pipes);
+
+        ListView pipelist = (ListView) findViewById(R.id.mainPipeList);
+        pipelist.setAdapter(adapter);
+    }
+}
---
+++ src/de/piepenmoker/app/PipeArrayAdapter.java
@@ -0,0 +1,99 @@
+package de.piepenmoker.app;
+
+import java.io.InputStream;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class PipeArrayAdapter extends ArrayAdapter<JSONObject> {
+
+    private Context parentContext;
+    private JSONObject[] pipes;
+
+    public PipeArrayAdapter(Context context, int resid, JSONObject[] objects ) {
+        super(context, resid, objects);
+
+        this.parentContext = context;
+        this.pipes = objects;
+    }
+
+    private class ViewHolder {
+        ImageView teaser;
+        TextView name;
+        TextView price;
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    } 
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder holder = null;
+        JSONObject pipe = pipes[position];
+
+        LayoutInflater mInflater = (LayoutInflater)
+                this.parentContext.getSystemService(
+                        Activity.LAYOUT_INFLATER_SERVICE);
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.pipe_item, null);
+            holder = new ViewHolder();
+            holder.name = (TextView) convertView.findViewById(R.id.pipeName);
+            holder.price = (TextView) convertView.findViewById(R.id.pipePrice);
+            holder.teaser = (ImageView) convertView.findViewById(
+                    R.id.pipeTeaser);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        try {
+            holder.name.setText(pipe.getString("name"));
+            holder.price.setText(pipe.getString("price") + " EUR");
+
+            String url = pipe.getString("teaser");
+            View target = convertView.findViewById(R.id.pipeTeaser);
+            new DownloadImageTask((ImageView)target).execute(url);
+
+        } catch (JSONException e) {
+
+        }
+        return convertView;
+
+	}
+}
+
